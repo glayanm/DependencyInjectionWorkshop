4f7df2368025570c93c3ad2ab9db9d11aff7a931
glayanm <anm821@gmail.com>
2019-09-21 14:26:13 +0800

    extract class NLogAdapter and inline LogMessage.

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,101 +1,17 @@
 ﻿using DependencyInjectionWorkshop.Models.Repository;
-using SlackAPI;
-using System;
-using System.Net.Http;
-using System.Text;
 
 namespace DependencyInjectionWorkshop.Models
 {
-    public class Sha256Adapter
+    public class NLogAdapter
     {
-        public Sha256Adapter()
+        public NLogAdapter()
         {
         }
 
-        public string GetHashedPassword(string password)
+        public void LogMessage(string message)
         {
-            var crypt = new System.Security.Cryptography.SHA256Managed();
-            var hash = new StringBuilder();
-            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
-            foreach (var theByte in crypto)
-            {
-                hash.Append(theByte.ToString("x2"));
-            }
-
-            var hashedPassword = hash.ToString();
-            return hashedPassword;
-        }
-    }
-
-    public class OtpService
-    {
-        public OtpService()
-        {
-        }
-
-        public string GetCurrentOtp(string accountId)
-        {
-            var response = new HttpClient() { BaseAddress = new Uri("http://joey.com/") }.PostAsJsonAsync("api/otps", accountId).Result;
-            if (response.IsSuccessStatusCode)
-            {
-                return response.Content.ReadAsAsync<string>().Result;
-            }
-            else
-            {
-                throw new Exception($"web api error, accountId:{accountId}");
-            }
-        }
-    }
-
-    public class SlackAdapter
-    {
-        public SlackAdapter()
-        {
-        }
-
-        public void Notify(string accountId)
-        {
-            var message = $"{accountId} try to login failed.";
-            var slackClient = new SlackClient("my api token");
-            slackClient.PostMessage(response1 => { }, "my channel", message, "my bot name");
-        }
-    }
-
-    public class FailedCounter
-    {
-        public FailedCounter()
-        {
-        }
-
-        public void AddFailedCount(string accountId)
-        {
-            var addFailedCountResponse = new HttpClient() { BaseAddress = new Uri("http://joey.com/") }.PostAsJsonAsync("api/failedCounter/Add", accountId).Result;
-            addFailedCountResponse.EnsureSuccessStatusCode();
-        }
-
-        public void ResetFailedCount(string accountId)
-        {
-            var resetResponse = new HttpClient() { BaseAddress = new Uri("http://joey.com/") }.PostAsJsonAsync("api/failedCounter/Reset", accountId).Result;
-            resetResponse.EnsureSuccessStatusCode();
-        }
-
-        public int GetFailedCount(string accountId)
-        {
-            var failedCountResponse =
-                new HttpClient() { BaseAddress = new Uri("http://joey.com/") }.PostAsJsonAsync("api/failedCounter/GetFailedCount", accountId).Result;
-            failedCountResponse.EnsureSuccessStatusCode();
-
-            var failedCount = failedCountResponse.Content.ReadAsAsync<int>().Result;
-            return failedCount;
-        }
-
-        public bool GetIsLocked(string accountId)
-        {
-            var isLockedResponse = new HttpClient() { BaseAddress = new Uri("http://joey.com/") }.PostAsJsonAsync("api/failedCounter/IsLocked", accountId).Result;
-
-            isLockedResponse.EnsureSuccessStatusCode();
-            var isLocked = isLockedResponse.Content.ReadAsAsync<bool>().Result;
-            return isLocked;
+            var logger = NLog.LogManager.GetCurrentClassLogger();
+            logger.Info(message);
         }
     }
 
@@ -106,6 +22,7 @@ namespace DependencyInjectionWorkshop.Models
         private readonly OtpService _otpService;
         private readonly SlackAdapter _slackAdapter;
         private readonly FailedCounter _failedCounter;
+        private readonly NLogAdapter _nLogAdapter;
 
         public AuthenticationService()
         {
@@ -114,6 +31,7 @@ namespace DependencyInjectionWorkshop.Models
             _otpService = new OtpService();
             _slackAdapter = new SlackAdapter();
             _failedCounter = new FailedCounter();
+            _nLogAdapter = new NLogAdapter();
         }
 
         public bool Verify(string accountId, string password, string otp)
@@ -140,28 +58,14 @@ namespace DependencyInjectionWorkshop.Models
             {
                 _failedCounter.AddFailedCount(accountId);
 
-                LogFailedCount(accountId);
+                var failedCount = _failedCounter.GetFailedCount(accountId);
+
+                _nLogAdapter.LogMessage($"accountId:{accountId} failed times:{failedCount}");
 
                 _slackAdapter.Notify(accountId);
 
                 return false;
             }
         }
-
-        private void LogFailedCount(string accountId)
-        {
-            var failedCount = _failedCounter.GetFailedCount(accountId);
-            LogMessage($"accountId:{accountId} failed times:{failedCount}");
-        }
-
-        private void LogMessage(string message)
-        {
-            var logger = NLog.LogManager.GetCurrentClassLogger();
-            logger.Info(message);
-        }
-    }
-
-    public class FailedTooManyTimesException : Exception
-    {
     }
 }
